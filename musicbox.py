92cc50da5769cc0bb51a0541c070f0c98a319843
Rachel Rayns <rach@rachelrayns.com>
2014-07-06 14:01:01 +0100

    Refactor Setup instruction into function

--- musicbox.py
+++ musicbox.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python2.7  
   
-import RPi.GPIO as GPIO 
-import time
+import RPi.GPIO as GPIO
 import pygame.mixer
 
 pygame.mixer.init(44000, -16, 2, 500) 
@@ -11,16 +10,6 @@ GPIO.setmode(GPIO.BCM)
 # 25 to stop the script  
 GPIO.setup(25, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) 
 
-# 3V3 (3.3V) to 17, 18, 27, 22, 23, 24 
-# to play notes  
-GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)  
-GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) 
-GPIO.setup(27, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) 
-GPIO.setup(22, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) 
-GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) 
-GPIO.setup(24, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-time_stamp = time.time()  
-
 # Point at sound files
 soundA = pygame.mixer.Sound("note_a.wav") 
 soundB = pygame.mixer.Sound("note_b.wav")
@@ -28,14 +17,6 @@ soundC = pygame.mixer.Sound("note_c.wav")
 soundD = pygame.mixer.Sound("note_d.wav") 
 soundE = pygame.mixer.Sound("note_e.wav")
 soundF = pygame.mixer.Sound("note_f.wav")
-
-# Loop all sounds
-soundA.play(loops=-1)
-soundB.play(loops=-1)
-soundC.play(loops=-1)
-soundD.play(loops=-1)
-soundE.play(loops=-1)
-soundF.play(loops=-1)
 
 SOUND_PINS = {
 	17: soundA,
@@ -46,36 +27,28 @@ SOUND_PINS = {
 	24: soundF
 }
 
-# Mute all Sounds
-soundA.set_volume(0.0)
-soundB.set_volume(0.0)
-soundC.set_volume(0.0)
-soundD.set_volume(0.0)
-soundE.set_volume(0.0)
-soundF.set_volume(0.0)
+# Define the setup function: activate pin, loop sound indefinitely, mute, rising edge is detected.
+def pin_sound_setup(pin):
+	GPIO.setup(pin, GPIO.IN, GPIO.PUD_DOWN)
+	sound = SOUND_PINS[pin]
+	sound.play(loops=-1)
+	sound.set_volume(0)
+	GPIO.add_event_detect(pin, GPIO.RISING, callback=note_on, bouncetime=200)
 
-# Define the callback functions: Turn Volume up to 100% 
-
-
+# Define the callback function: Turn Volume up to 100% 
 def note_on(pin):
 	sound = SOUND_PINS[pin]
 	sound.set_volume(1.0)
 	print "Note %s playing" % pin
-  
-# The GPIO.add_event_detect() line below set things up so that  
-# when a rising edge is detected, regardless of whatever   
-# else is happening in the program, the notes will be run  
-# It will happen even while the program is waiting for  
-# a raising edge on Pin 25.  
 
-GPIO.add_event_detect(17, GPIO.RISING, callback=note_on, bouncetime=200)  
-GPIO.add_event_detect(18, GPIO.RISING, callback=note_on, bouncetime=200)
-GPIO.add_event_detect(27, GPIO.RISING, callback=note_on, bouncetime=200)  
-GPIO.add_event_detect(22, GPIO.RISING, callback=note_on, bouncetime=200)  
-GPIO.add_event_detect(23, GPIO.RISING, callback=note_on, bouncetime=200)  
-GPIO.add_event_detect(24, GPIO.RISING, callback=note_on, bouncetime=200)   
- 
-print "Pin 25 pressed - script end."
-GPIO.wait_for_edge(25, GPIO.RISING)
+def main():
+	for pin in SOUND_PINS:
+		pin_sound_setup(pin)
+	  
+	print "Waiting for Pin 25 to end script."
+	GPIO.wait_for_edge(25, GPIO.RISING)
 
-GPIO.cleanup()
+	GPIO.cleanup()
+
+if __name__ == "__main__":
+	main()

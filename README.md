b3849de9752e35f950cf59d2dc22b223677bfa04
Ben Nuttall <ben@bennuttall.com>
2014-11-28 22:31:02 +0000

    New version

--- README.md
+++ README.md
@@ -1,33 +1,3 @@
-Music Box
-=================
+# Music Box
 
-#### New install of Raspian
-
-#### Wiring
-
-##### Raspberry Pi:
-
-**Inputs (expecting 3.3V):**
-
-GPIO 17
-
-GPIO 18
-
-GPIO 27
-
-GPIO 22
-
-GPIO 23
-
-GPIO 24
-
-**Speaker:**
-
-Either 3.5mm jack or USB + 3.5mm jack Adafruit Audio Card.
-
-
-
-
-#### Install Adafruit Audio Card (if using USB sound card)
-
-https://learn.adafruit.com/usb-audio-cards-with-a-raspberry-pi/cm108-type
+Play a sound when a button is pressed. Buttons mapped to particular sounds.
--- musicbox.py
+++ musicbox.py
@@ -1,51 +1,42 @@
-#!/usr/bin/env python2.7
-
 import RPi.GPIO as GPIO
 import pygame.mixer
 
 pygame.mixer.init(44000, -16, 2, 500)
 GPIO.setmode(GPIO.BCM)
 
-# 25 to stop the script
-GPIO.setup(25, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+# Pin 25 stops the script
+GPIO.setup(25, GPIO.IN, GPIO.PUD_DOWN)
 
-# Point at sound files
-soundA = pygame.mixer.Sound("note_a.wav")
-soundB = pygame.mixer.Sound("note_b.wav")
-soundC = pygame.mixer.Sound("note_c.wav")
-soundD = pygame.mixer.Sound("note_d.wav")
-soundE = pygame.mixer.Sound("note_e.wav")
-soundF = pygame.mixer.Sound("note_f.wav")
+sound_a = pygame.mixer.Sound("sounds/note_a.wav")
+sound_b = pygame.mixer.Sound("sounds/note_b.wav")
+sound_c = pygame.mixer.Sound("sounds/note_c.wav")
+sound_d = pygame.mixer.Sound("sounds/note_d.wav")
+sound_e = pygame.mixer.Sound("sounds/note_e.wav")
+sound_f = pygame.mixer.Sound("sounds/note_f.wav")
 
-SOUND_PINS = {
-    17: soundA,
-    18: soundB,
-    27: soundC,
-    22: soundD,
-    23: soundE,
-    24: soundF
+sound_pins = {
+    17: sound_a,
+    18: sound_b,
+    27: sound_c,
+    22: sound_d,
+    23: sound_e,
+    24: sound_f,
 }
 
-# Define the setup function: activate pin, loop sound
-# indefinitely, mute, GPIO event detection
+def setup_pin(pin):
     GPIO.setup(pin, GPIO.IN, GPIO.PUD_DOWN)
-    sound = SOUND_PINS[pin]
-    sound.play(loops=-1)
-    sound.set_volume(0)
-    GPIO.add_event_detect(pin, GPIO.BOTH, callback=pin_change, bouncetime=0)
 
-# Define the callback function: Set volume according to GPIO pin status
-def pin_change(pin):
-    sound = SOUND_PINS[pin]
-    volume = GPIO.input(pin)
-    sound.set_volume(volume)
-    state = "high" if volume else "low"
-    print "Pin %s is %s" % (pin, state)
+def play(pin):
+    sound = sound_pins[pin]
+    sound.play()
+    print("Playing note %s" % pin)
 
 def main():
-    map(pin_change, SOUND_PINS)
+    for pin in sound_pins:
+        setup_pin(pin)
+        GPIO.add_event_detect(pin, GPIO.FALLING, callback=play, bouncetime=1000)
 
-    print "Waiting for Pin 25 to end script."
+    print("Waiting for Pin 25 to end script")
     GPIO.wait_for_edge(25, GPIO.RISING)
 
     GPIO.cleanup()

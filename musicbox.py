bb1da17fbcd43b499a19b01cf4130ad8226eb7dd
Ben Nuttall <ben@bennuttall.com>
2014-12-02 18:13:18 +0000

    New version

--- musicbox.py
+++ musicbox.py
@@ -1,11 +1,9 @@
-import RPi.GPIO as GPIO
 import pygame.mixer
+import RPi.GPIO as GPIO
 
-pygame.mixer.init(44000, -16, 2, 500)
+pygame.mixer.init()
 GPIO.setmode(GPIO.BCM)
-
-# Pin 25 stops the script
-GPIO.setup(25, GPIO.IN, GPIO.PUD_DOWN)
+GPIO.setwarnings(False)
 
 sound_a = pygame.mixer.Sound("sounds/note_a.wav")
 sound_b = pygame.mixer.Sound("sounds/note_b.wav")
@@ -15,31 +13,25 @@ sound_e = pygame.mixer.Sound("sounds/note_e.wav")
 sound_f = pygame.mixer.Sound("sounds/note_f.wav")
 
 sound_pins = {
-    17: sound_a,
-    18: sound_b,
-    27: sound_c,
-    22: sound_d,
-    23: sound_e,
-    24: sound_f,
+    2: sound_a,
+    3: sound_b,
+    4: sound_c,
+    14: sound_d,
+    15: sound_e,
+    17: sound_f,
 }
-
-def setup_pin(pin):
-    GPIO.setup(pin, GPIO.IN, GPIO.PUD_DOWN)
 
 def play(pin):
     sound = sound_pins[pin]
     sound.play()
-    print("Playing note %s" % pin)
+    print("playing note from pin %s" % pin)
 
 def main():
     for pin in sound_pins:
-        setup_pin(pin)
-        GPIO.add_event_detect(pin, GPIO.FALLING, callback=play, bouncetime=1000)
+        GPIO.setup(pin, GPIO.IN, GPIO.PUD_DOWN)
+        GPIO.add_event_detect(pin, GPIO.FALLING, play, 1000)
 
-    print("Waiting for Pin 25 to end script")
-    GPIO.wait_for_edge(25, GPIO.RISING)
-
-    GPIO.cleanup()
+    print("waiting for button press")
 
 if __name__ == "__main__":
     main()
